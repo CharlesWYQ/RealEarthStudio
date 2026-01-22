a7c8c330416c2510d00f3337b7706b7cb33ba26c
CharlesWYQ <charleswyq@foxmail.com>
2026-01-22 22:36:31 +0800

    修改fiftyone启动方式

--- utils/fifty_one/start_app.py
+++ utils/fifty_one/start_app.py
@@ -7,13 +7,26 @@
 # @Details : 启动App
 
 import fiftyone as fo
-import time
+import time, os
 
 
 def start_fiftyone_app():
+    host = os.getenv("FIFTYONE_HOST", "0.0.0.0")
+    port = int(os.getenv("FIFTYONE_PORT", "5151"))
+
     while True:
         try:
-            session = fo.launch_app(dataset=None, auto=False, port=5151)
+            session = fo.launch_app(
+                dataset=None,
+                auto=False,  # 不自动打开浏览器
+                port=port,  # 指定端口
+                address=host,  # 绑定到所有网络接口
+                remote=True  # 允许远程访问
+            )
+
+            print(f"FiftyOne 应用程序已在 http://{host}:{port} 启动")
+            print(f"可通过外部终端访问: http://<your_server_ip>:{port}")
+
             session.wait()
         except KeyboardInterrupt:
             print("\n应用程序已退出")
